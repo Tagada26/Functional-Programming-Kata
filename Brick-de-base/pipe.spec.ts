e7d5a199b684626fb4174151a0deb1d794b1949b
Marjorie <aubertmarjorie2@gmail.com>
2023-11-16 14:04:32 +0100

    🥝 pipe

--- Brick-de-base/pipe.spec.ts
+++ Brick-de-base/pipe.spec.ts
@@ -8,9 +8,12 @@ function multiply2(num: number): number {
   return num * 2
 }
 
-describe('', () => {
-    it('', () => {
-        const result = add1(2)
+// Fonctionnement pipe : A -> (A->B) -> (B->C) -> (C->D)
+
+describe('Pipe', () => {
+    it('Compare procédural and functional way', () => {
+        const num = add1(1)
+        const result = multiply2(num)
         const result2 = pipe(
             1,
             add1, 
@@ -19,7 +22,7 @@ describe('', () => {
         
         console.log(result)
         console.log(result2)
-        expect(result2).toEqual(4)
+        expect(result2).toEqual(result)
     })
 })
 
